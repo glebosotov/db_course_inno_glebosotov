7a52b6b687328ea5956781e709ccacb514177d69
Gleb <gleb.osotov@gmail.com>
2021-03-02 10:47:50 +0300

    lab7 ex1.1 , ex2.1

---
+++ lab7/lab7.sql
@@ -0,0 +1,6 @@
+select distinct film.title, film.rating, category.name from film JOIN film_category ON film.film_id = film_category.film_id JOIN category ON film_category.category_id = category.category_id JOIN inventory ON film.film_id = inventory.film_id JOIN rental ON inventory.inventory_id = rental.inventory_id  WHERE (film.rating = 'R' OR film.rating = 'PG-13') AND (category.name = 'Horror' OR category.name = 'Sci-Fi') AND rental.return_date is not null
+
+
+explain analyze select distinct film.title, film.rating, category.name from film JOIN film_category ON film.film_id = film_category.film_id JOIN category ON film_category.category_id = category.category_id JOIN inventory ON film.film_id = inventory.film_id JOIN rental ON inventory.inventory_id = rental.inventory_id  WHERE (film.rating = 'R' OR film.rating = 'PG-13') AND (category.name = 'Horror' OR category.name = 'Sci-Fi') AND rental.return_date is not null
+-- the most costly operation is JOIN and the easiest way to make it better is to create indeces.
+-- in particular scanning rental database is expensive since a single movie can appear multiple times there
